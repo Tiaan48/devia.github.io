5c8ef77149309a5b660dcd410c68b80d3f72fa8d
Tiaan48 <32510195+Tiaan48@users.noreply.github.com>
2017-10-10 10:45:23 +0200

    ico

--- js/custom.js
+++ js/custom.js
@@ -61,155 +61,249 @@ jQuery(document).ready(function($){
 });
 
 // ICO behaviour
-var myConfig = {
-  "type":"pie",
-  "title":{
-    "text":"Animated Pie Chart"
-  },
-  "legend":{
-    "x":"75%",
-    "y":"25%",
-    "border-width":1,
-    "border-color":"gray",
-    "border-radius":"5px",
-    "header":{
-      "text":"Legend",
-      "font-family":"Georgia",
-      "font-size":12,
-      "font-color":"#3333cc",
-      "font-weight":"normal"
+var originalConfig =     {
+    "type":"ring",
+    "title":{
+      "text":"Drilldown Pie"
     },
-    "marker":{
-      "type":"circle"
+    "plot":{
+        "detach":false,
+        "cursor":"hande",
+        "shadow":8,
+        "tooltip":{
+            "visible":false
+        },
+        "animation":{
+            "delay":10,
+            "effect":"2",
+            "speed":"ANIMATION_FAST",
+            "method":"1",
+            "sequence":"3",
+            "attributes":{
+                
+            }
+        },
+        "value-box":{
+            "color":"#FFF",
+            "text":"%t",
+            "font-weight":"none",
+            "font-size":14
+        }
     },
-    "toggle-action":"remove",
-    "minimize":true,
-    "icon":{
-      "line-color":"#9999ff"
-    },
-    "max-items":8,
-    "overflow":"scroll"
-  },
-  "plotarea":{
-    "margin-right":"30%",
-    "margin-top":"15%"
-  },
-  "plot":{
-    "animation":{
- 	    "on-legend-toggle": true, //set to true to show animation and false to turn off
- 	    "effect": 5,
- 	    "method": 1,
- 	    "sequence": 1,
- 	    "speed": 1
-    },
-    "value-box":{
-      "text":"%v",
-      "font-size":12,
-      "font-family":"Georgia",
-      "font-weight":"normal",
-          "placement":"out",
-          "font-color":"gray",
-    },
-    "tooltip":{
-      "text":"%t: %v (%npv%)",
-      "font-color":"black",
-      "font-family":"Georgia",
-      "text-alpha":1,
-      "background-color":"white",
-      "alpha":0.7,
-      "border-width":1,
-      "border-color":"#cccccc",
-      "line-style":"dotted",
-      "border-radius":"10px",
-      "padding":"10%",
-      "placement":"node:center"
-    },
-    "border-width":1,
-    "border-color":"#cccccc",
-    "line-style":"dotted"
-  },
-  "series":[
-    {
-      "values":[234],
-      "background-color":"#cc0000",
-      "text":"Alpha"
-    },
-    {
-      "values":[121],
-      "background-color":"#ff3300",
-      "text":"Beta"
-    },
-    {
-      "values":[77],
-      "background-color":"#ff6600",
-      "text":"Gamma"
-    },
-    {
-      "values":[65],
-      "background-color":"#ff9933",
-      "text":"Delta"
-    },
-    {
-      "values":[59],
-      "background-color":"#ffcc00",
-      "text":"Epsilon"
-    },
-    {
-      "values":[35],
-      "background-color":"#ace600",
-      "text":"Zeta"
-    },
-    {
-      "values":[34],
-      "background-color":"#88cc00",
-      "text":"Eta"
-    },
-    {
-      "values":[31],
-      "background-color":"#339933",
-      "text":"Theta"
-    },
-    {
-      "values":[16],
-      "background-color":"#66ccff",
-      "text":"Iota"
-    },
-    {
-      "values":[14],
-      "background-color":"#3399ff",
-      "text":"Kappa"
-    },
-    {
-      "values":[13],
-      "background-color":"#0066ff",
-      "text":"Lambda"
-    },
-    {
-      "values":[12],
-      "background-color":"#3333cc",
-      "text":"Mu"
-    },
-    {
-      "values":[11],
-      "background-color":"#6600ff",
-      "text":"Nu"
-    },
-    {
-      "values":[10],
-      "background-color":"#9933ff",
-      "text":"Xi"
-    },
-    {
-      "values":[9],
-      "background-color":"#9999ff",
-      "text":"Omicron"
-    }
-  ]
-};
+    "series":[
+        {
+            "values":[69],
+            "background-color":"#F44336",
+            "text":"Visualization<br>Tools",
+            "data-id":"vt"
+        },
+        {
+            "values":[69],
+            "background-color":"#009688",
+            "text":"Site<br>Performance",
+            "data-id":"sp"
+        },
+        {
+            "values":[69],
+            "background-color":"#00BCD4",
+            "text":"Dev<br>Tools",
+            "data-id":"dt"
+        },
+        {
+            "values":[69],
+            "background-color":"#03A9F4",
+            "text":"Security<br>Tools",
+            "data-id":"st"
+        },
+        {
+            "values":[69],
+            "background-color":"#673AB7",           
+            "text":"Data<br>Management",
+            "data-id":"dm"
+        }
+    ]
+}
  
 zingchart.render({ 
 	id : 'myChart', 
-	data : myConfig, 
-	height: 400, 
-	width: "100%" 
+	data : originalConfig, 
+	height: 450, 
+	width: '100%' 
 });
+ 
+ 
+ 
+/************************
+ *  Secondary Charts
+ * *********************/
+var drilldownConfig = {
+    "type":"bar",
+    "title":{
+        "text":"Security Tools"
+    },
+    "plotarea": {
+        "margin":"dynamic"
+    },
+    "plot":{
+        "animation":{
+            "delay":10,
+            "effect":"4",
+            "speed":"1200",
+            "method":"1",
+            "sequence":"3"
+        },
+        "tooltip":{
+            "text": "Quantity: %v",
+            "shadow":true,
+            "shadowAlpha":.5,
+            "shadowBlur":2,
+            "shadowDistance":3,
+            "shadowColor":"#c4c4c4",
+            "borderWidth":0,
+            "font-size":18
+        }
+    },
+    "series":[
+        {
+            "values":[35,15,25,10],
+            "styles":["#1565C0","#42A5F5","#1E88E5","#90CAF9"]
+        }
+    ],
+    "scale-x":{
+        "line-color":"#555",
+        "tick":{
+            "line-color":"#555"
+        },
+        "values":["Firewall","Cache-control","Link-access","HTTP-Comp"],
+        "item":{
+        	"max-chars":9,
+            "color":"#555",
+            "font-size":12
+        },
+        "label":{
+            "text":"Type",
+            "color":"#555",
+            "font-weight":"none",
+            "font-size":16
+        }
+    },
+    "scale-y":{
+        "line-color":"#555",
+        "tick":{
+            "line-color":"#555"
+        },
+        "item":{
+            "color":"#555",
+            "font-size":12
+        },
+        "guide":{
+            "visible":false
+        },
+        "label":{
+            "text":"Quantity",
+            "color":"#555",
+            "font-weight":"none",
+            "font-size":16
+        }
+    },
+    "shapes":[
+       {
+        'x':20,
+        'y':20,
+        'size':10,
+        'angle':-90,
+        'type':'triangle',
+        'background-color':'#C4C4C4',
+        'padding':5,
+        'cursor':'hand',
+        'id': 'backwards'
+      }
+    ]
+};
+ 
+/**
+ * Create associative array to manage drilldown config
+ */
+var drilldownDataStructure = [];
+drilldownDataStructure["vt"] = {
+    "data":[10,25,35],
+    "scale-labels":["Grid-component","Map-tool","Web-charting"],
+    "title":"Visualization Tools",
+    "colors":["#EF5350","#E53935","#C62828"]
+};
+drilldownDataStructure["sp"] = {
+    "data":[15,5,35,20],
+    "scale-labels":["Speed-test","Error-tracking","Load-testing","User-monitoring"],
+    "title":"Site Performance",
+    "colors":["#26A69A","#80CBC4","#00695C","#00897B"]
+};
+drilldownDataStructure["dt"] = {
+    "data":[20,8,35,20],
+    "scale-labels":["IDE","File-Management","Image-Generation","QA-testing"],
+    "title":"Dev Tools",
+    "colors":['#26C6DA','#80DEEA','#00838F','#00ACC1']
+};
+drilldownDataStructure["st"] = {
+    "data":[35,15,25,10],
+    "scale-labels":["Firewall","Cache-control","Link-access","HTTP-Comp"],
+    "title":"Security Tools",
+    "colors":["#1565C0","#42A5F5","#1E88E5","#90CAF9"]
+};
+drilldownDataStructure["dm"] = {
+    "data":[10,25,35],
+    "scale-labels":["Relational","Non-relational","Cluster"],
+    "title":"Data Management",
+    "colors":["#5E35B1","#4527A0","#7E57C2"]
+};
+ 
+ 
+/*
+* Built in zingchart API event used to capture node click events.
+* Starting from this scope you will handle drilldown functionality.
+*/
+zingchart.node_click = function(p) {
+  var plotIndex = p.plotindex;
+  var scaleText = p.scaletext;
+   
+  // You could use this data to help construct drilldown graphs check it out...
+  //console.log(p);
+  if (drilldownDataStructure[p['data-id']]) {
+    drilldownConfig['title']['text'] = drilldownDataStructure[p['data-id']]['title'];
+    drilldownConfig['scale-x']['values'] = drilldownDataStructure[p['data-id']]['scale-labels'];
+    drilldownConfig['series'][0]['values'] = drilldownDataStructure[p['data-id']]['data'];
+    drilldownConfig['series'][0]['styles'] = drilldownDataStructure[p['data-id']]['colors'];
+    zingchart.exec('myChart', 'destroy');
+    zingchart.render({
+    	id : 'drilldown1', 
+    	data : drilldownConfig, 
+      height: 450, 
+    	width: '100%' 
+    });
+  }
+}
+ 
+/*
+* Handle history buttons. You can assign
+* shapes id's and based on id you can go 
+* 'forward' or 'backwards'. You could 
+* also handle this with HTML and register
+* click events to those DOM elements.
+*/
+zingchart.shape_click = function(p) {
+  var shapeId = p.shapeid;
+  //console.log(p);
+  
+  switch(shapeId) {
+    case 'forwards':
+    case 'backwards':
+    case 'default':
+      zingchart.exec('drilldown1', 'destroy');
+      zingchart.render({
+      	id : 'myChart', 
+      	data : originalConfig, 
+        height: 450, 
+      	width: '100%' 
+      });
+      break;
+  }
+}
